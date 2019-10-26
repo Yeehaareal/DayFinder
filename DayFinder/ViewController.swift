2c2db246b5b2e4328b9d12943dc12314e0c7f2ed
Didzis Tupureins <d.tupureins@inbox.lv>
2019-10-26 11:57:37 +0300

    Initial Commit

---
+++ DayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by Didzis Tupureins on 26/10/2019.
+//  Copyright © 2019 Didzis Tupureins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
